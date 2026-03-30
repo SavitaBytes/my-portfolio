cee4c57cf06716024c239c6e8a38a50346b9014a
savita <pantsavita5@gmail.com>
2026-03-30 14:20:43 +0530

    changed the vite-config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',  // must match your GitHub repo name
+  base: '/',  // must match your GitHub repo name
 })
